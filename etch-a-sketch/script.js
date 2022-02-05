f90b3cc834a5955cf4c6b2983ee09fd97e9869fe
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-02-06 00:54:59 +0530

    Added the multi color functionality and the color changes to the black.

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -2,6 +2,12 @@ const container = document.querySelector('.container');
 const resetButton = document.querySelector('.reset');
 const size = document.getElementById('size').value;
 const newBox = document.querySelector('input');
+const rainbowButton = document.querySelector('.rainbow-button');
+const blackButton = document.querySelector('.black-button')
+const counter = 0;
+if (rainbowButton.onclick === true) {
+    counter = counter + 1;
+}
 
 function makeGrid(size) {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -10,12 +16,17 @@ function makeGrid(size) {
         const boxes = document.createElement('div');
         boxes.classList.add("grid-element");
         boxes.addEventListener('mouseover', () => {
-                boxes.style.backgroundColor = "black";
-            })
-        
+            boxes.style.backgroundColor = "black";
+        })
+        const randomNumber = Math.floor(Math.random() * 100);
+        boxes.addEventListener('mouseover', () => {
+        boxes.style.backgroundColor = `rgb(${randomNumber-10}% ${randomNumber-10}% ${randomNumber-10}%)`;
+        })
         container.appendChild(boxes);
-    }
-}
+        }    
+}     
+
+
 
 
 resetButton.onclick = (e) => {
@@ -26,10 +37,22 @@ resetButton.onclick = (e) => {
         makeGrid(size);
     } else {
         makeGrid(size)
-    }
-    
+    } 
 }
 
+rainbowButton.onclick = () => {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size * size; i++) {
+        const boxes = document.createElement('div');
+        boxes.classList.add("grid-element");
+        const randomNumber = Math.floor(Math.random() * 100);
+        boxes.addEventListener('mouseover', () => {
+            boxes.style.backgroundColor = `rgb(${randomNumber-10}% ${randomNumber-10}% ${randomNumber-10}%)`;
+        })
+        container.appendChild(boxes);
+}
+}
 
 makeGrid(size)
  
