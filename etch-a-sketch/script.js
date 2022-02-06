deebb2f643a5e0954d6deb6b867cb21afe6bee7b
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-02-06 22:10:16 +0530

    Code is updated hence bug got fixed by adding default state to our code.

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,13 +1,34 @@
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = "black";
+
+let currentColor = DEFAULT_COLOR;
+let currentSize = DEFAULT_SIZE;
+
 const container = document.querySelector('.container');
 const resetButton = document.querySelector('.reset');
-const size = document.getElementById('size').value;
+const size = document.getElementById('size');
 const newBox = document.querySelector('input');
 const rainbowButton = document.querySelector('.rainbow-button');
-const blackButton = document.querySelector('.black-button')
-const counter = 0;
-if (rainbowButton.onclick === true) {
-    counter = counter + 1;
+const blackButton = document.querySelector('.black-button');
+
+rainbowButton.onclick = () => setColor('rainbow');
+blackButton.onclick = () => setColor('black');
+size.onchange = (e) => setSize(e.target.value);
+resetButton.onclick = () => reloadGrid();
+
+
+function setColor(newColor) {
+    currentColor = newColor;
+    reloadGrid();
 }
+
+function setSize(newSize) {
+    currentSize = newSize;
+    reloadGrid();
+}
+
+
+
 
 function makeGrid(size) {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -15,52 +36,39 @@ function makeGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const boxes = document.createElement('div');
         boxes.classList.add("grid-element");
-        boxes.addEventListener('mouseover', () => {
-            boxes.style.backgroundColor = "black";
-        })
-        const randomNumber = Math.floor(Math.random() * 100);
-        boxes.addEventListener('mouseover', () => {
-        boxes.style.backgroundColor = `rgb(${randomNumber-10}% ${randomNumber-10}% ${randomNumber-10}%)`;
-        })
+        boxes.addEventListener('mouseover', setFinalColor)
         container.appendChild(boxes);
-        }    
-}     
+    }    
+} 
 
 
-
-
-resetButton.onclick = (e) => {
-    container.innerHTML = "";
-    let size = prompt("Please enter the size of grid which you desire")
-    if (size === "") {
-        const size = document.getElementById('size').value;
-        makeGrid(size);
+function setFinalColor() {
+    if (currentColor === 'rainbow') {
+        const rRandomNum = Math.floor(Math.random() * 100);
+        const gRandomNum = Math.floor(Math.random() * 100);
+        const bRandomNum = Math.floor(Math.random() * 100);
+        this.style.backgroundColor = `rgb(${rRandomNum-10}% ${gRandomNum-10}% ${bRandomNum-10}%)`;
     } else {
-        makeGrid(size)
-    } 
+        this.style.backgroundColor = currentColor;
+    }
 }
 
-rainbowButton.onclick = () => {
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    for (let i = 0; i < size * size; i++) {
-        const boxes = document.createElement('div');
-        boxes.classList.add("grid-element");
-        const randomNumber = Math.floor(Math.random() * 100);
-        boxes.addEventListener('mouseover', () => {
-            boxes.style.backgroundColor = `rgb(${randomNumber-10}% ${randomNumber-10}% ${randomNumber-10}%)`;
-        })
-        container.appendChild(boxes);
-}
+function clearGrid() {
+    container.innerHTML = "";
 }
 
-makeGrid(size)
- 
-newBox.addEventListener('change', () => {
-    const size = document.getElementById('size').value;
-    makeGrid(size);
+function reloadGrid() {
     clearGrid();
-})
+    makeGrid(currentSize);
+}
+
+    
+
+
+
+
+
+makeGrid(currentSize);
 
 
 
