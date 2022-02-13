2f8ac688036e71dd3a1311faebac414efffba2ba
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-02-14 00:21:01 +0530

    Calculator is working fine with this updated code.

--- calculator/script.js
+++ calculator/script.js
@@ -1,27 +1,59 @@
 const DEFAULT_DISPLAY_VALUE = 0;
+const DEFAULT_EXPRESSION_VALUE = 0;
+const expressionObject = {
+    operator: ['+', '-', '/', '*', "="],
+    num1: [0,1,2,3,4,5,6,7,8,9],
+    num2: [0,1,2,3,4,5,6,7,8,9],
+}
 
+let currentExpression = [];
 
 let currentDisplayValue = DEFAULT_DISPLAY_VALUE;
+let currentExpressionValue = DEFAULT_EXPRESSION_VALUE;
 
 const pressedButtonValue = document.querySelectorAll('button');
 const displayResult = document.querySelector('.display-result');
-const displayExpression = document.querySelector('display-expression');
+const displayExpression = document.querySelector('.display-expression');
+const activeButtons = document.getElementsByClassName('active');
+const clearButton = document.querySelector('.clear');
+const equalButton = document.querySelector('.equal');
 
 function setDefaultResult(event) {
     if (event.innerText === '' || 0) {
         event.innerText = currentDisplayValue;
     } else {
         event.addEventListener('click', function() {
-            displayResult.innerText = this.innerText
+            if (this.innerText === ('clear' || 'delete')) {
+                clearButton.addEventListener('click', () => {
+                    displayResult.innerHTML = 0;
+                    displayExpression.innerHTML= '&nbsp;';
+                });
+            } else if(this.innerText === '='){
+                equalButton.addEventListener('click', () => {
+                    console.log(currentExpression);
+                    displayResult.innerText = operator(currentExpression[1], +currentExpression[0], +currentExpression[2]);
+                });
+            } else {
+                displayResult.innerText = this.innerText;
+                currentDisplayValue = displayResult.innerText;
+                displayExpression.innerText = displayExpression.innerText + displayResult.innerText;
+            }    
          });
+    }  
+}
+
+function setDefaultExpression(event) {
+    if (event.innerText === '' || 0) {
+        event.innerText = currentExpressionValue;
     }
 }
 
-setDefaultResult(displayResult);
-
-function setDisplayResult() {
-    displayResult.innerText = event.innerText;
+function joinTheExpression() {
 }
+
+setDefaultResult(displayResult);
+setDefaultExpression(displayExpression);
+
 
 function add (a, b) {
     return a + b;
@@ -51,10 +83,33 @@ function operator(operator, num1, num2) {
     }
 }
 
+function activateButton() {
+    this.classList.add('active');
+}
+
+function deactivateButton() {
+    this.classList.remove('active');
+}
+
+
+
 pressedButtonValue.forEach(button => {
     button.addEventListener('click',setDefaultResult(button));
 })
 
+pressedButtonValue.forEach(button => {
+    button.addEventListener('click',activateButton);
+})
 
 
+pressedButtonValue.forEach(button => {
+    button.addEventListener('click', () => {
+        for (props of activeButtons) {
+            currentExpression.push(props.innerText);
+        }
+    })
+})
 
+pressedButtonValue.forEach(button => {
+    button.addEventListener('click',deactivateButton);
+})
