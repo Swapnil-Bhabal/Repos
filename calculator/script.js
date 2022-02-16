235de005de6103d64c304e71bac51891cf4cee68
Swapnil Bhabal <swapnilbhabal787@gmail.com>
2022-02-17 00:48:38 +0530

    Chaining of expression is done as desired.

--- calculator/script.js
+++ calculator/script.js
@@ -32,6 +32,7 @@ let operand1 = '';
 let operand2 = '';
 let currentOperator = '';
 let currentExpression = [];
+let currentResult = '';
 
 const allButtons = document.querySelectorAll('button');
 const displayResult = document.querySelector('.display-result');
@@ -42,35 +43,71 @@ const equalButton = document.querySelector('.equal');
 const operators = document.querySelectorAll('.operator');
 const numbers = document.querySelectorAll('.number');
 
-allButtons.forEach(button => button.addEventListener('click', (e) => {
-    console.log(e.target.textContent)
-}));
 
-numbers.forEach(number => number.addEventListener('click', (e) => {
+function clearAll() {
+    displayResult.innerText = '';
+    displayExpression.innerText = '';
+    operand1 = '';
+    currentOperator = '';
+    operand2 = '';
+    currentResult = '';
+}
+
+function resetValues() {
+    operand1 = '';
+    currentOperator = '';
+    operand2 = '';
+}
+
+function setOperator(e) {
+    currentOperator = e.target.textContent;
+    displayResult.innerText = currentOperator;
+    displayExpression.innerText += currentOperator;
+}
+
+function setNumber(e) {
     if (currentOperator === '') {
         operand1 += e.target.textContent;
         displayResult.innerText = operand1;
+        displayExpression.innerText = operand1;
+        
     } else {
         operand2 += e.target.textContent;
         displayResult.innerText = operand2;
+        displayExpression.innerText += operand2;
     }
+}
+
+function operation() {
+    currentResult = operator(currentOperator, operand1, operand2); 
+    displayResult.innerText = currentResult;
+     
+}
+
+
+numbers.forEach(number => number.addEventListener('click', (e) => {
+    setNumber(e);
 }));
 
 
 operators.forEach(operator => operator.addEventListener('click', (e) => {
-    currentOperator = e.target.textContent;
-    displayResult.innerText += currentOperator;
+    setOperator(e);
+    if (currentResult !== '') {
+        operand1 = currentResult
+        displayExpression.innerText = operand1;
+        displayExpression.innerText += currentOperator;
+        operand2 = '';
+        operation();
+    } else {
+        operand2 = '';
+        operation();
+    }
 }));
 
 
-equalButton.addEventListener('click', () => {
-    displayResult.innerText = operator(currentOperator, operand1, operand2);
+equalButton.addEventListener('click', (e) => {
+    operation();
+    resetValues();
 });
 
-clearButton.addEventListener('click', () => {
-    displayResult.innerText = '';
-    operand1 = '';
-    currentOperator = '';
-    operand2 = '';
-});
-
+clearButton.addEventListener('click', clearAll);
